b0dc8b4ad7492136a7eeaf1f2bffa27c42170e83
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-08-24 22:12:07 +0500

    Initial commit

---
+++ FirstProgram.py
@@ -0,0 +1,35 @@
+print("hello world")
+
+name = "haseeb"
+age = 19
+price = 25.65
+
+print ("my name is :", name)
+print ("my age is :", age)
+
+print (type(name))
+print (type(age))
+print (type(price))
+
+a = 10
+b = 25
+sum = a + b
+print (sum)
+
+print (not False)
+print (not (a > b))
+
+val1 = True
+val2 = False
+
+print ("and operator :", val1 and val2)
+print ("or operator :", val1 or val2)
+print ("or operator", (a == b) or (b > a))
+
+name = input ("Enter your name : ")
+age = int(input ("Enter your age : "))
+marks = float(input ("Enter your marks : "))
+
+print (type(name),"Welcome", name)
+print (type(age),"Your age is : ",age)
+print (type(marks),"Your marks are : ",marks)
